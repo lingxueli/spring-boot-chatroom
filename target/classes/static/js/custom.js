b0558ba182227bc86ea1dc8e22d6862883e58880
lingxueli <ll2871@columbia.edu>
2019-09-03 23:15:42 -0600

    update static content path

--- target/classes/static/js/custom.js
+++ target/classes/static/js/custom.js
@@ -5,7 +5,7 @@ function login() {
     // TODO: complete the login function
     user = document.getElementById('username').value
     console.log(user)
-    location.replace('/index?username)
+    //location.replace('/index?username)
 }
 
 /**
