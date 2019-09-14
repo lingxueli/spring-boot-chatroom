b461c13dd2cfea3449c2790bd55223dacf40a708
lingxueli <ll2871@columbia.edu>
2019-09-14 16:45:33 -0600

    add hamcrest dependency to overwrite error

--- src/main/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/main/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
