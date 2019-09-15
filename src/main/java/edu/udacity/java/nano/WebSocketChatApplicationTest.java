6233e49541b55f08f3d8f30fcb8d8ac04d526b3c
lingxueli <ll2871@columbia.edu>
2019-09-14 19:52:15 -0600

    fix build error

--- src/main/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/main/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -1,7 +1,10 @@
 package edu.udacity.java.nano;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,6 +12,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest
 class WebSocketChatApplicationTest {
     @Autowired
     private MockMvc mockMvc;
